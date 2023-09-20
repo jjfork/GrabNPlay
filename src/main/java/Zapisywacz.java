c27f9b16a692b5a429250bd4c7c0e92e75d40041
jjfork <jakubiakrjulian@gmail.com>
2023-09-20 22:59:11 +0200

    modified method

--- src/main/java/Zapisywacz.java
+++ src/main/java/Zapisywacz.java
@@ -17,66 +17,66 @@ public class Zapisywacz {
             login(page);
             regis(page);
 //1 Modelowanie wolumetryczne obiektów przestrzennych [W06GIT-SI0025L]
-            register(page, "#W06GIT-SI0025L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0025L");
             pick_group(page, 1);
             confirm(page);
             go_back(page);
 
 //2 Modelowanie wolumetryczne obiektów przestrzennych [W06GIT-SI0025W]
-            register(page, "#W06GIT-SI0025W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0025W");
 
 //3 Podstawy geofizyki [W06GIT-SI0021L]
-            register(page, "#W06GIT-SI0021L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0021L");
             pick_group(page,2);
             confirm(page);
             go_back(page);
 
 
 //4 Podstawy geofizyki [W06GIT-SI0021C]
-            register(page, "#W06GIT-SI0021C > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0021C");
 
 //5 Podstawy geofizyki [W06GIT-SI0021W]
-            register(page, "#W06GIT-SI0021W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0021W");
 
 
 
 //6 Podstawy przetwarzania geodanych [W06GIT-SI0024L]
-            register(page, "#W06GIT-SI0024L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0024L");
             pick_group(page,1);
             confirm(page);
             go_back(page);
 
 
 //7 Podstawy przetwarzania geodanych [W06GIT-SI0024W]
-            register(page, "#W06GIT-SI0024W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0024W");
 
 //8 Systemy geoinformacyjne [W06GIT-SI0026L]
-            register(page, "#W06GIT-SI0026L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0026L");
             pick_group(page,2);
             confirm(page);
             go_back(page);
 
 
 //9 Systemy geoinformacyjne [W06GIT-SI0026W]
-            register(page, "#W06GIT-SI0026W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0026W");
 
 //10 Wstęp do uczenia maszynowego [W06GIT-SI0022L]
-            register(page, "#W06GIT-SI0022L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0022L");
             pick_group(page,1);
             confirm(page);
             go_back(page);
 
 //11 Wstęp do uczenia maszynowego [W06GIT-SI0022W]
-            register(page, "#W06GIT-SI0022W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0022W");
 
 //12 Zastosowania GIS w naukach o Ziemi [W06GIT-SI0023L]
-            register(page, "#W06GIT-SI0023L > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0023L");
             pick_group(page,1);
             confirm(page);
             go_back(page);
 
 //13 Zastosowania GIS w naukach o Ziemi [W06GIT-SI0023W]
-            register(page, "#W06GIT-SI0023W > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)");
+            register(page, "#W06GIT-SI0023W");
 
             context.tracing().stop(new Tracing.StopOptions()
                     .setPath(Paths.get("trace.zip")));
@@ -99,7 +99,7 @@ public class Zapisywacz {
     }
 
     private static void register(Page page, String selektor_grupa) {
-        page.locator(selektor_grupa).click();
+        page.locator(selektor_grupa + " > td:nth-child(4) > span:nth-child(3) > a:nth-child(3)").click();
     }
 
     private static void pick_group(Page page, int numerGrupy) {
